a91da0598be055c50a3f1babec011f9cf2f9108e
creaxaura <creaxaura@gmail.com>
2023-12-01 20:25:29 -0700

    dropdown test
    testing dropdown

---
+++ javascript/open-dropdown.js
@@ -0,0 +1,16 @@
+function dropdownVisiblity(){
+  var content = document.getElementByClass("dropdown-options");
+  if(content[0].style.display == "none"){
+    content[0].style.display = "grid"
+    var dropUL = document.getElementById("drop-ul");
+    dropUL.innerHTML = `<li><a href="#">Home</a></li>
+    <li><a href="portfolio.html">Portfolio</a></li>
+    <li><a href="about.html">About</a></li>
+    <li><a href="commissions.html">Commissions</a></li>
+    <li><a href="projects.html">Projects</a></li>
+    <li><a href="stream-info.html">Stream Info</a></li>
+    <li><a href="stickers.html">Shop</a></li>`
+  } else {
+    content[0].style.display = "none"
+  }
+  }
